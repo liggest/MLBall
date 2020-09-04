6473f462a62613cf36a50821b37fc46f4141a684
liggest <liggest@sina.cn>
2020-09-04 10:24:58 +0800

    初始commit
    喂仓库文件

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Transform owner;
+    public float rotateSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (owner)
+        {
+            //transform.Rotate(owner.up + owner.position, rotateSpeed * Time.fixedDeltaTime, Space.Self);
+            transform.RotateAround(owner.position, owner.up, rotateSpeed * Time.fixedDeltaTime);
+            Debug.Log($"{ owner.position},{owner.up}");
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Test/PlayerAgent.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Policies;
+
+public class PlayerAgent : Agent // <- 注意这里是Agent
+{
+
+    public float[] viewDegrees;
+    public float maxDistance;
+
+    Rigidbody rig;
+    BehaviorParameters bp;
+
+    private void Awake()
+    {
+        bp = GetComponent<BehaviorParameters>();
+
+        int defaultSize = bp.BrainParameters.VectorObservationSize;
+        int actualSize = 5 + 4 * viewDegrees.Length;
+        if (defaultSize != actualSize)
+        {
+            bp.BrainParameters.VectorObservationSize = actualSize;
+            Debug.Log("正在观测的参数数量与设置中不符，已改正");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (float degree in viewDegrees)
+        {
+            Vector3 direction = Quaternion.AngleAxis(degree, transform.up) * transform.forward;
+            Debug.DrawRay(transform.position, direction * maxDistance, Color.white);
+        }
+    }
+
+    public override void OnEpisodeBegin()  // 每个周期开始时 重置场景
+    {
+            
+    }
+
+    public override void CollectObservations(VectorSensor sensor) // 向网络提供数据
+    {
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(rig.velocity.x);
+        sensor.AddObservation(rig.velocity.z);
+
+        foreach (float degree in viewDegrees)
+        {
+            
+            int hitType = -1;
+            Vector3 hitPos = Vector3.zero;
+            Vector3 direction = Quaternion.AngleAxis(degree, transform.up) * transform.forward;
+            if (Physics.Raycast(transform.localPosition, direction, out RaycastHit info, maxDistance)) 
+            {   
+                if (info.collider.CompareTag("Player"))
+                {
+                    hitType = 1;
+                }
+                else if (info.collider.CompareTag("Ball"))
+                {
+                    hitType = 2;
+                }
+                else if (info.collider.CompareTag("Wall"))
+                {
+                    hitType = 0;
+                }
+                hitPos = info.transform.InverseTransformPoint(info.point);
+            }
+            sensor.AddObservation(hitType);
+            sensor.AddObservation(hitPos);
+        }
+
+    }
+
+    public override void OnActionReceived(float[] vectorAction) // 得到网络输出 指定权重
+    {
+        float moveX = vectorAction[0];
+        float moveY = vectorAction[1];
+
+        float shootX = vectorAction[2];
+        float shootY = vectorAction[3];
+
+        if (false)
+        {
+            SetReward(0.0f); // 设置奖励
+            EndEpisode();  //结束当前周期
+        }
+    }
+
+    public override void Heuristic(float[] actionsOut) // 手操
+    {
+        actionsOut[0] = Input.GetAxis("Horizontal");
+        actionsOut[1] = Input.GetAxis("Vertical");
+
+        actionsOut[2] = Input.GetAxis("Horizontal");
+        actionsOut[3] = Input.GetAxis("Vertical");
+
+    }
+
+}
