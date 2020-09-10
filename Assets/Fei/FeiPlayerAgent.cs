ac27e6d8fc8ad7ee632572c94b3615b134a5e945
duolanda <duolanda@live.cn>
2020-09-10 16:37:31 +0800

    模仿学习4B+SelfPlay

--- Assets/Fei/FeiPlayerAgent.cs
+++ Assets/Fei/FeiPlayerAgent.cs
@@ -31,14 +31,17 @@ public class FeiPlayerAgent : PlayerAgent
         SetReward(-1f);
     }
 
-    public override void ShootReward(float forceValue)
-    {
-        AddReward(0.25f);
-    }
+    //public override void ShootReward(float forceValue)
+    //{
+    //    AddReward(0.25f);
+    //}
 
     public override void KeepBallReward()
     {
-        AddReward(0.001f);
+        //AddReward(0.001f);
+        if (KeepBallTime > 5f) {
+            AddReward(-0.000001f);
+        }
     }
 
     public override void GoalReward(Goal g, Ball b)
@@ -52,30 +55,30 @@ public class FeiPlayerAgent : PlayerAgent
             AddReward(-0.8f);
         }
     }
-    public override void IdleReward()
-    {
-        AddReward(-0.0009f);
-    }
+    //public override void IdleReward()
+    //{
+    //    AddReward(-0.0009f);
+    //}
 
-    public override void ObservationReward(int observeType, Vector3 observePos, float distance)
-    {
-        if (observeType == 1)
-        {
-            AddReward(0.0005f);
-        }
-        if (distance > 1.8f && distance < 8f)
-        {
-            AddReward(1 / distance / 5000);
-        }
-        //else if (distance > 8f)
-        //{
-        //    AddReward(-distance / 1000);
-        //}
+    //public override void ObservationReward(int observeType, Vector3 observePos, float distance)
+    //{
+    //    if (observeType == 1)
+    //    {
+    //        AddReward(0.0005f);
+    //    }
+    //    if (distance > 1.8f && distance < 8f)
+    //    {
+    //        AddReward(1 / distance / 5000);
+    //    }
+    //    //else if (distance > 8f)
+    //    //{
+    //    //    AddReward(-distance / 1000);
+    //    //}
 
-        //}
-        //public override void BumpPlayerReward(Transform playerTransform)
-        //{
-        //        AddReward(-0.001f);
-        //}
-    }
+    //    //}
+    //    //public override void BumpPlayerReward(Transform playerTransform)
+    //    //{
+    //    //        AddReward(-0.001f);
+    //    //}
+    //}
 }
