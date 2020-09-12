e5479b5f71d4abf6e3aeb57f2072c6c7307b7c81
duolanda <duolanda@live.cn>
2020-09-12 11:53:02 +0800

    让它射2！

--- Assets/Fei/FeiPlayerAgent.cs
+++ Assets/Fei/FeiPlayerAgent.cs
@@ -4,84 +4,84 @@ using UnityEngine;
 
 public class FeiPlayerAgent : PlayerAgent
 {
-    //public override void BumpWallReward()
+    ////public override void BumpWallReward()
+    ////{
+    ////    AddReward(-0.1f);
+    ////}
+
+    //public override void GetBallReward()
     //{
-    //    AddReward(-0.1f);
+    //    if (CurrentBall.lastPlayer == null)
+    //    {
+    //        AddReward(0.01f);
+    //    }else if (IsTeammate(CurrentBall.lastPlayer))
+    //    {
+    //        AddReward(0.012f);
+    //        SM.AddTeamReward(TeamName, 0.002f);
+    //    }
+    //    else
+    //    {
+    //        AddReward(0.02f);
+    //    }
+
     //}
 
-    public override void GetBallReward()
-    {
-        if (CurrentBall.lastPlayer == null)
-        {
-            AddReward(0.01f);
-        }else if (IsTeammate(CurrentBall.lastPlayer))
-        {
-            AddReward(0.012f);
-            SM.AddTeamReward(TeamName, 0.002f);
-        }
-        else
-        {
-            AddReward(0.02f);
-        }
-
-    }
-
-    public override void FallReward()
-    {
-        SetReward(-1f);
-    }
-
-    public override void ShootReward(float forceValue)
-    {
-        AddReward(0.32f);
-    }
-
-    public override void KeepBallReward()
-    {
-        if (KeepBallTime > 3f) {
-            AddReward(-0.000001f);
-        }
-        else
-        {
-            AddReward(0.001f);
-        }
-    }
-
-    public override void GoalReward(Goal g, Ball b)
-    {
-        if (g.IsRivalGoal(b))
-        {
-            AddReward(1f);
-        }
-        else
-        {
-            AddReward(-0.8f);
-        }
-    }
-    //public override void IdleReward()
+    //public override void FallReward()
     //{
-    //    AddReward(-0.0009f);
+    //    SetReward(-1f);
     //}
 
-    public override void ObservationReward(int observeType, Vector3 observePos, float distance)
-    {
-        if (observeType == 1)
-        {
-            AddReward(0.0005f);
-        }
-        if (distance > 1.8f && distance < 8f)
-        {
-            AddReward(1 / distance / 5000);
-        }
-        //else if (distance > 8f)
-        //{
-        //    AddReward(-distance / 1000);
-        //}
+    //public override void ShootReward(float forceValue)
+    //{
+    //    AddReward(0.32f);
+    //}
 
-        //    //}
-        //    //public override void BumpPlayerReward(Transform playerTransform)
-        //    //{
-        //    //        AddReward(-0.001f);
-        //    //}
-    }
+    //public override void KeepBallReward()
+    //{
+    //    if (KeepBallTime > 3f) {
+    //        AddReward(-0.000001f);
+    //    }
+    //    else
+    //    {
+    //        AddReward(0.001f);
+    //    }
+    //}
+
+    //public override void GoalReward(Goal g, Ball b)
+    //{
+    //    if (g.IsRivalGoal(b))
+    //    {
+    //        AddReward(1f);
+    //    }
+    //    else
+    //    {
+    //        AddReward(-0.8f);
+    //    }
+    //}
+    ////public override void IdleReward()
+    ////{
+    ////    AddReward(-0.0009f);
+    ////}
+
+    //public override void ObservationReward(int observeType, Vector3 observePos, float distance)
+    //{
+    //    if (observeType == 1)
+    //    {
+    //        AddReward(0.0005f);
+    //    }
+    //    if (distance > 1.8f && distance < 8f)
+    //    {
+    //        AddReward(1 / distance / 5000);
+    //    }
+    //    //else if (distance > 8f)
+    //    //{
+    //    //    AddReward(-distance / 1000);
+    //    //}
+
+    //    //    //}
+    //    //    //public override void BumpPlayerReward(Transform playerTransform)
+    //    //    //{
+    //    //    //        AddReward(-0.001f);
+    //    //    //}
+    //}
 }
